57d0a0b3ac90fa47c62ec76fd2e0816a8d24f3d1
Marian Stoica <marian.stoica@web-prime.ro>
2018-02-18 00:20:06 +0200

    Comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppShellComponent } from './app-shell/app-shell.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+/**
+ * TODO:
+ * remove AppShellComponent and `shell` route
+ * after https://github.com/angular/angular-cli/issues/8929 gets fixed
+ * WORKAROUND: import AppShellComponent
+ */
+import { AppShellComponent } from './app-shell/app-shell.component';
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  /**
+   * TODO:
+   * remove AppShellComponent and `shell` route
+   * after https://github.com/angular/angular-cli/issues/8929 gets fixed
+   * WORKAROUND: define `shell` route below
+   */
   { path: 'shell', component: AppShellComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
