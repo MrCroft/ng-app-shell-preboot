7c3aa96f595b56638edb7e69324fe1828c56a21e
Marian Stoica <marian.stoica@web-prime.ro>
2018-02-22 18:33:54 +0200

    Removed Api request from the HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ApiService } from '../core/services/api.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  videos = this.api.getVideos();
 
-  constructor(
-    private api: ApiService
-  ) {}
+  constructor() {}
 
   ngOnInit() {}
 
